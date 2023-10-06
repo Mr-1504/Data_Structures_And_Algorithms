c4e20c19429055c0b14d52b87b1481de12d70f0f
Mr-1504 <128713637+Mr-1504@users.noreply.github.com>
2023-10-06 22:06:06 +0700

    Create node_iterator.cpp

---
+++ ung_dung_linked_list/node_iterator.cpp
@@ -0,0 +1,36 @@
+#include "node.cpp"
+#ifndef _iterator_CPP
+#define _iterator_CPP
+using namespace std;
+template <class T>
+class iter
+{
+    node<T> *a;
+
+public:
+    iter() { a = nullptr; }
+    iter(node<T> *x) { a = x; }
+    node<T> *getnode() { return a; }
+    iter<T> &operator=(iter<T> x)
+    {
+        this->a = x.getnode();
+        return *this;
+    }
+    bool operator!=(iter<T> x) { return a != x.getnode(); }
+    iter<T> operator++()
+    {
+        a->getNext();
+        return a;
+    }
+    iter<T> operator++(int)
+    {
+        node<T> *temp;
+        a = a->getNext();
+        return temp;
+    }
+    T &operator*()
+    {
+        return a->getE();
+    }
+};
+#endif
