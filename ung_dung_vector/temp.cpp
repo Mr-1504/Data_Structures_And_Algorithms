2417b7070a181e0c2537eba3fccbf7b3259b26c4
Mr-1504 <128713637+Mr-1504@users.noreply.github.com>
2023-10-06 22:06:36 +0700

    Create temp.cpp

---
+++ ung_dung_vector/temp.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include "vector_moi.cpp"
+using namespace std;
+int main(){
+    vector_moi<int> a;
+    a.them_cuoi(2);
+    a.them_cuoi(1);
+    sort(a.begin(), a.end());
+}
