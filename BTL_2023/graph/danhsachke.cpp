e1894549ebcc2c451c376ea4a72b2a78d6dab0b9
Mr-1504 <128713637+Mr-1504@users.noreply.github.com>
2023-10-09 23:05:08 +0700

    update

--- BTL_2023/graph/danhsachke.cpp
+++ BTL_2023/graph/danhsachke.cpp
@@ -6,36 +6,121 @@ class adj_list
 {
     int n;
     Don_list<int> *a;
-    int trongso;
+    int *trongso;
+
 public:
     adj_list(int n)
     {
         this->n = n;
         this->a = new Don_list<int>[n];
-        for(int i=0;i<n;i++){
+        this->trongso = new int[n * n];
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
             a[i].push_back(i);
+            trongso[i * n + j++] = 0;
         }
     }
-    void add(int s, int d)
+    ~adj_list()
+    {
+        delete[] a;
+        delete[] trongso;
+    }
+    // int &operator [](int k){
+    //     return a[k].front();
+    // }
+    void add(int s, int d, int tso)
     {
         a[s].push_back(d);
         a[d].push_back(s);
+        trongso[s * n + d] = trongso[d * n + s] = tso;
     }
-    void out()
+    friend ifstream &operator>>(ifstream &cin, adj_list &x)
     {
-        
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < x.n; i++)
         {
-            cout<<"dinh "<<a[i].front()<<" ket noi: ";
-            node<int> *temp = a[i].dau().getnode()->getNext();
-            while (temp!=nullptr)
+            int s, d, tso;
+            cin >> s >> d >> tso;
+            x.add(s, d, tso);
+        }
+        cout << "Doc thanh cong!\n";
+        return cin;
+    }
+    friend istream &operator>>(istream &cin, adj_list &x)
+    {
+        for (int i = 0; i < x.n; i++)
+        {
+            int s, d, tso;
+            cin >> s >> d >> tso;
+            x.add(s, d, tso);
+        }
+        cout << "Doc thanh cong!\n";
+        return cin;
+    }
+    friend ofstream &operator<<(ofstream &cout, const adj_list &x)
+    {
+        cout << "Số đỉnh: " << x.n << endl;
+        for (int i = 0; i < x.n; i++)
+        {
+            cout << "Đỉnh " << x.a[i].front() << " kết nối với:\n";
+            node<int> *temp = x.a[i].dau().getnode()->getNext();
+            int j = 1;
+            while (temp != nullptr)
             {
-                cout<<temp->getE()<<" ";
-                temp= temp->getNext();
+                cout << "\tĐỉnh " << temp->getE() << " có trọng số " << x.trongso[i * x.n + j] << endl;
+                temp = temp->getNext();
+                j++;
             }
             cout << endl;
         }
+        return cout;
     }
-
+    friend ostream &operator<<(ostream &cout, const adj_list &x)
+    {
+        cout << "So dinh: " << x.n << endl;
+        for (int i = 0; i < x.n; i++)
+        {
+            int j=1;
+            cout << "Dinh " << x.a[i].front() << " ket noi voi:\n";
+            node<int> *temp = x.a[i].dau().getnode()->getNext();
+            while (temp != nullptr)
+            {
+                cout <<"\tDinh "<< temp->getE() << " co trong so "<< x.trongso[i*x.n +j]<<endl;
+                temp = temp->getNext();
+                j++;
+            }
+            cout << endl;
+        }
+        return cout;
+    }
+    void dfs(int u, int t[])
+    {
+        if (!t[u])
+        {
+            t[u] = 1;
+            node<int> *temp = a[u].dau().getnode()->getNext();
+            while (temp != nullptr)
+            {
+                int v = temp->getE();
+                if (!t[v])
+                    dfs(v, t);
+                temp = temp->getNext();
+            }
+        }
+    }
+    int dem_lien_thong()
+    {
+        int test[n] = {0};
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!test[i])
+            {
+                res++;
+                dfs(i, test);
+            }
+        }
+        return res;
+    }
 };
 #endif
--- BTL_2023/graph/demo.cpp
+++ BTL_2023/graph/demo.cpp
@@ -1,10 +1,13 @@
 #include "danhsachke.cpp"
 using namespace std;
 int main(){
-    adj_list a(4);
-    a.add(0,1);
-    a.add(0,2);
-    a.add(0,3);
-    a.add(1,2);
-    a.out();
+    ifstream input("input.txt");
+    int n;
+    input>>n;
+    adj_list a(n);
+    input>>a;
+    ofstream output("output.txt");
+    output<<a;
+    cout<<a;
+    cout<<a.dem_lien_thong();
 }
